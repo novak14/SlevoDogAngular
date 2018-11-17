4847bfae3f600b3b22c951577c8c11b3e6e084e4
Marcel Novák <marcel.novak14@live.com>
2018-11-17 15:24:03 +0100

    comments and test project

--- Catalog/Dal/Entities/Sale.cs
+++ Catalog/Dal/Entities/Sale.cs
@@ -7,24 +7,90 @@ namespace Catalog.Dal.Entities
 {
     public class Sale
     {
+        /// <summary>
+        /// Unique identification of object
+        /// </summary>
         [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Name of sale
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Price of sale after sale
+        /// </summary>
         public decimal PriceAfterSale { get; set; }
+
+        /// <summary>
+        /// Average price of sale
+        /// </summary>
         public decimal AveragePrice { get; set; }
+
+        /// <summary>
+        /// Origin price of sale
+        /// </summary>
         public decimal OriginPrice { get; set; }
-        public string Image { get; set; }
-        public DateTime DateInsert { get; set; }
-        public DateTime ValidFrom { get; set; }
-        public DateTime ValidTo { get; set; }
-        public string LinkFirm { get; set; }
-        public string Description { get; set; }
-        public bool bDisabled { get; set; }
+
+        /// <summary>
+        /// Percentage sale of sale
+        /// </summary>
         public decimal PercentSale { get; set; }
+
+        /// <summary>
+        /// Image of sale
+        /// </summary>
+        public string Image { get; set; }
+
+        /// <summary>
+        /// Date Insert of sale
+        /// </summary>
+        public DateTime DateInsert { get; set; }
+
+        /// <summary>
+        /// From DateTime when is sale valid
+        /// </summary>
+        public DateTime ValidFrom { get; set; }
+
+        /// <summary>
+        /// To DateTime is valid sale
+        /// </summary>
+        public DateTime ValidTo { get; set; }
+
+        /// <summary>
+        /// For creating link to company(firm)
+        /// </summary>
+        public string LinkFirm { get; set; }
+
+        /// <summary>
+        /// Description of sale
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Check if Sale is disabled and if can be shown on website
+        /// </summary>
+        public bool bDisabled { get; set; }
+
+        /// <summary>
+        /// Overall rank of the sale
+        /// </summary>
         public int RankSale { get; set; }
+
+        /// <summary>
+        /// Foreign key to shop
+        /// </summary>
         public int FkShop { get; set; }
 
+        /// <summary>
+        /// Collectioon of comments
+        /// </summary>
         public List<Comments> Comments { get; set; } = new List<Comments>();
+
+        /// <summary>
+        /// Navigation property to cateogory
+        /// </summary>
         public Category Category { get; set; }
     }
 }
--- Catalog/Dal/Entities/User.cs
+++ Catalog/Dal/Entities/User.cs
@@ -6,8 +6,19 @@ namespace Catalog.Dal.Entities
 {
     public class User
     {
+        /// <summary>
+        /// Unique identification of user
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Username of user
+        /// </summary>
         public string UserName { get; set; }
+
+        /// <summary>
+        /// Id in AspNetUser, used for identification
+        /// </summary>
         public string UniqueString { get; set; }
     }
 }
